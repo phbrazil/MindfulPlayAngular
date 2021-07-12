a4a8dd151e1e84b02994456fef0097ddbed4bf27
Paulo <pauloh2012sul@gmail.compwd>
2021-07-12 14:12:13 -0300

    blog

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -11,10 +11,32 @@ export class BlogComponent implements OnInit {
 
   public links = ['./assets/open-graph-test/medium-angular.json'];
 
+   blogImages = [
+     {"imageName": "foto 1.jpg", "title": "Titulo Noticia 1"},
+     {"imageName": "foto 2.jpg", "title": "Titulo Noticia 2"},
+     {"imageName": "foto 3.jpg", "title": "Titulo Noticia 3"},
+     {"imageName": "foto 4.jpg", "title": "Titulo Noticia 4"},
+     {"imageName": "foto 5.jpg", "title": "Titulo Noticia 5"},
+     {"imageName": "foto 6.jpg", "title": "Titulo Noticia 6"},
+     {"imageName": "foto 7.jpg", "title": "Titulo Noticia 7"},
+     {"imageName": "foto 8.jpg", "title": "Titulo Noticia 8"},
+     {"imageName": "foto 9.jpg", "title": "Titulo Noticia 9"},
+     {"imageName": "foto 10.jpg", "title": "Titulo Noticia 10"},
+
+
+    ];
+
+  
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+
+    this.blogImages.forEach(element => {
+
+      console.log(element.imageName)
+      
+    });
 
   }
 
